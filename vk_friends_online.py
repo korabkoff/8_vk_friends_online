93c0657242f33023f247f6ebbd39b46b8195603d
korabkoff <korabkoff@gmail.com>
2019-01-09 16:06:51 +0300

    define type of exception in 'Get_blacklist_from_url()'

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,30 +1,49 @@
 import vk
+import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
+APP_ID = 6807175  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 def get_user_login():
-    pass
+    try:
+        login = input('Login: ')
+        return login
+
+    except ValueError:
+        print("Invalid input")
+        return None
 
 
 def get_user_password():
-    pass
+    try:
+        password = getpass()
+        return password
+
+    except ValueError:
+        print("Invalid input")
+        return None
 
 
 def get_online_friends(login, password):
+    if not login or not password:
+        return None
+
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
     )
     api = vk.API(session)
-    # например, api.friends.get()
-
+    vk_friends = api.friends.get.fields(status)
+    return friends_online
 
 def output_friends_to_console(friends_online):
-    pass
+    print([friend_online for friend_online in friends_online])
+    # return friends_online
 
+
+
+    return parser.parse_args(args)
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
